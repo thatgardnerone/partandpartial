ed531808f85369e5d5715c74f715dfe1fc5bebb9
Jamie Gardner <jamie.gardner@visualsoft.co.uk>
2023-09-22 15:44:05 +0100

    Add some content to the default layout

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,5 +1,21 @@
 <script setup></script>
 
 <template>
-    <slot/>
+    <div class="min-h-screen flex flex-col bg-gray-50">
+        <header
+            class="sticky top-0 flex-0 px-4 py-6 bg-gray-50 border-b border-gray-200">
+            <h1 class="font-semibold text-2xl">Part and Partial</h1>
+        </header>
+
+        <div class="flex-1">
+            <slot />
+        </div>
+
+        <footer
+            class="sticky bottom-0 flex-0 px-4 py-6 bg-gray-50 border-t border-gray-200">
+            <p class="text-gray-600">
+                &copy; {{ new Date().getFullYear() }} TGO Solutions
+            </p>
+        </footer>
+    </div>
 </template>
