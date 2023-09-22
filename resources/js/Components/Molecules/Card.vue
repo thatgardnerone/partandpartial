f982c9d250f65908ca789a707b110c5d9036424d
Jamie Gardner <jamie.gardner@visualsoft.co.uk>
2023-09-22 16:12:04 +0100

    Display loading text by default

---
+++ resources/js/Components/Molecules/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ArrowPathIcon } from "@heroicons/vue/24/outline/index.js";
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: Object,
+        default: null,
+    },
+});
+</script>
+
+<template>
+    <div
+        class="flex aspect-square flex-1 flex-col justify-between bg-gray-100 p-4">
+        <div class="flex-0 flex items-center justify-between">
+            <h2 class="font-semibold uppercase text-gray-600">
+                {{ title }}
+            </h2>
+            <div>
+                <ArrowPathIcon class="mx-auto h-6 w-6 text-gray-400" />
+            </div>
+        </div>
+        <div class="flex flex-1 flex-col items-center justify-center">
+            <p
+                :class="{
+                    'animate-pulse text-gray-500': !content,
+                }">
+                {{ content ?? "loading" }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- resources/js/Pages/Demo.vue
+++ resources/js/Pages/Demo.vue
@@ -1,5 +1,18 @@
 <script setup>
-import {Head} from '@inertiajs/vue3';
+import { Head } from "@inertiajs/vue3";
+import Card from "@/Components/Molecules/Card.vue";
+import { ref } from "vue";
+
+const props = defineProps({
+    slowData: {
+        type: Array,
+        required: true,
+    },
+});
+
+const item0 = ref(props.slowData?.[0]);
+const item1 = ref(props.slowData?.[1]);
+const item2 = ref(props.slowData?.[2]);
 </script>
 
 <template>
@@ -7,10 +20,15 @@ import {Head} from '@inertiajs/vue3';
         <title>Heavy components</title>
     </Head>
 
-    <h1>Heavy components</h1>
-    <p>Coming soon...</p>
+    <main class="mx-auto flex max-w-4xl flex-col px-4 py-6">
+        <h2 class="my-4 text-xl font-bold">Heavy components</h2>
+
+        <div class="flex gap-x-5">
+            <Card title="Card 1" :content="item0" />
+            <Card title="Card 2" :content="item1" />
+            <Card title="Card 3" :content="item2" />
+        </div>
+    </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
