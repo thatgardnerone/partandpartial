28b2b27dbd13a265d1af6d49a9536874bfaf1f44
Jamie Gardner <jamie.gardner@visualsoft.co.uk>
2023-09-22 12:23:51 +0100

    Strip back to basics

---
+++ resources/js/Pages/Demo.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {Head} from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head>
+        <title>Heavy components</title>
+    </Head>
+
+    <h1>Heavy components</h1>
+    <p>Coming soon...</p>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,29 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import DefaultLayout from '@/Layouts/DefaultLayout.vue';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Part and Partial';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        const page = resolvePageComponent(`./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'));
+
+        page.then((module) => {
+           const page = module.default;
+           let layout = page.layout;
+
+           if (typeof layout === 'undefined') {
+               layout = DefaultLayout;
+           }
+
+           page.layout = layout;
+        });
+
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
