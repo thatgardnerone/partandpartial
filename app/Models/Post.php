b0887e596524ee8def4972c55f72a8f7aacd0841
Jamie Gardner <jamie.gardner@visualsoft.co.uk>
2023-10-24 17:53:29 +0100

    Create and seed posts

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'deleted_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
